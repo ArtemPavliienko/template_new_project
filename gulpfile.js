d8be23891b4ae1f40714edaaf7df503bf21f6e4b
ArtemPavliienko <kvazarmw@gmail.com>
2018-02-01 17:45:55 +0200

    Template new project

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-csso'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        server: './project'
+    });
+
+    gulp.watch('./project/*.sass',['sass']);
+    gulp.watch('./project/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    gulp.watch('./project/*.sass', ['sass']);
+    gulp.src('./project/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 4 versions']
+        }))
+        .pipe(gulp.dest('./project/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('css', function() { 
+  gulp.src('./project/css/style.css')
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./project/css/'))
+});
+
+
+
+gulp.task('default', ['serve']);
